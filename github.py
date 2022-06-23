4ac68dab8ac804bbc215498fffd18a22f6addc3f
Cristian Nunes <crisjav2812@gmail.com>
2022-06-23 14:14:25 -0300

    a ver si va

---
+++ github.py
@@ -0,0 +1,7 @@
+#github es como una red social de programadores
+#subimos proyectos, y si damos los permisos, colaboradores aportan
+#creación de un repositorio: 
+#copio el link de la cración del respositorio.
+#en la consola conecto el repositorio de github con el de git
+#comando git remote add origin y link.
+#usamos git push origin y la rama que queremos, en este caso el master
